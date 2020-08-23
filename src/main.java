ceba5ff4bff84cb038d2b3d1d02a5c11b826b17c
Eyal Delarea <Eyaldelarea@gmail.com>
2020-08-23 14:09:02 +0300

    Change interface

--- src/main.java
+++ src/main.java
@@ -1,17 +1,23 @@
 
 public class main {
     public static void main(String[] args) {
-        String s = "C:\\Users\\eyald\\IdeaProjects\\Gzip\\src\\exampleInputs\\OnTheOrigin.txt";
-        String t = "C:\\Users\\eyald\\Desktop\\smiley.bmp";
-        String a
-                = "C:\\Users\\eyald\\Desktop\\Red_Flowers.bmp";
-        String d = "C:\\Users\\eyald\\Desktop\\CompressedFile";
-        String e = "C:\\Users\\eyald\\Desktop\\originalFile";
 
 
-        Lz77EncoderDecoder l = new Lz77EncoderDecoder(8, 1024);
-        l.CompressLz(a, d);
-        l.deCompress(d, e);
+        //Please change your base path
+        final String basePath = "C:\\Users\\eyald\\Desktop\\";
+
+        final String fileToCompressPath = basePath+"Romeo and Juliet  Entire Play.txt"; //file to compress
+        final String compressOutPutPath = basePath+"CompressedFile";                    //where to save compress
+        final String finalOutPutPath = basePath+"originalFile";                         //where to save decompress
+        final int searchBufferSize = 64;
+        final int windowSize = 2048;
+
+
+        Lz77EncoderDecoder l = new Lz77EncoderDecoder(searchBufferSize, windowSize);
+
+
+        l.CompressLz(fileToCompressPath, compressOutPutPath);
+        l.deCompress(compressOutPutPath, finalOutPutPath);
 
 
     }
